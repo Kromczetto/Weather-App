a6fe8189e23ed4f0fb6251f8e8f45c8d719cd8f3
Kromczetto <jakubkromolowski@gmail.com>
2025-09-07 19:52:19 +0200

    Add geolocation features and update weather page to display current location weather

--- weatherapp/lib/pages/your_location_weather_page.dart
+++ weatherapp/lib/pages/your_location_weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/services/weather_service.dart';
+import 'package:weatherapp/models/weather_model.dart';
 
 class LocationWeather extends StatefulWidget {
   const LocationWeather({super.key});
@@ -8,10 +10,48 @@ class LocationWeather extends StatefulWidget {
 }
 
 class _LocationWeatherState extends State<LocationWeather> {
+  final _weatherService = WeatherService('f9b9e140a2be4b58b2d134424250609');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+    print(cityName);
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+  @override 
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Location Weather")
+      body: SafeArea(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              }, 
+              child: Text("Go Back"), 
+              ),
+            if (_weather != null) ...[
+              Text(
+              _weather!.cityName,
+              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              )
+            ] else ...[
+              const CircularProgressIndicator(),
+            ]
+        ],)
+      )
     );
   }
 }
--- weatherapp/lib/services/weather_service.dart
+++ weatherapp/lib/services/weather_service.dart
@@ -1,5 +1,8 @@
 import 'dart:convert';
 
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
 import '../models/weather_model.dart';
 import '../models/weather_forecast_model.dart';
 import 'package:http/http.dart' as http;
@@ -28,5 +31,21 @@ class WeatherService {
     } else {
       throw Exception('Failed to load weather forecast data');
     }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied');
+      }
+    }
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    String? city = placemarks.first.locality;
+
+    return city ?? "Unknown";
   }
 }
